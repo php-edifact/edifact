81f46a5ed031c5d11963efe3883b53bf8f43bf19
sabas <sabas88@gmail.com>
2013-12-19 13:24:41 +0100

    Update class.EDIParser.php

--- class.EDIParser.php
+++ class.EDIParser.php
@@ -1,15 +1,22 @@
 <?php
 /*
 EDIFACT messages parser
+
 Input a valid path to an edi file
+	$c=new EDIParser('name.edi');
+	or
+	$c=new EDIParser();
+	$c->parse('name.edi');
 Output a multidimensional array.
+	Errors $c->errors()
+	Array  $c->get()
+	Json $c->getJson()
 */
 
 class EDIParser
 {
 	public $parsedfile;
-	private $obj;
-	public $errors;
+	private $errors;
 
 	public function __construct($url=null)
 	{
@@ -42,41 +49,53 @@ class EDIParser
 			$line = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $line); //basic sanitization, remove non printable chars
 			
 			$line = preg_replace('#[\r\n]#', '', $line); //carriage return removal (CR+LF)
-		
 			if (strlen($line)==0) {
-				unset($file2[$x]);
-				continue;
+			unset($file2[$x]);
+			continue;
 			}
 			if (strrpos($line,"'")!=strlen($line)-1) 
-				$this->errors[]='Segment not ended correctly at line '.$i. "=>". $line;
+					$this->errors[]='Segment not ended correctly at line '.$i. " => ". $line;
 			$line=$this->splitSegment($line);
 		}
 		$this->parsedfile=$file2;
 		return $file2;
 	}
 	
-	function splitSegment($str) //segment
+	private function splitSegment($str) //segment
 	{
 		$str = strrev(str_replace("'","",strrev($str)));//remove ending " ' "
 		$matches=preg_split("~\?[:'\+\?]{1}(*SKIP)(*FAIL)|\+~s", $str); //split except release character
-		
-		if(strpos($matches[0],'UNA')!==FALSE) return['UNA',substr($str,3)]; //ignore UNA segment (TODO actually use it)
-
 		foreach ($matches as &$value)
 		{ 
-			if (preg_match("~\?[:'\+\?]{1}(*SKIP)(*FAIL)|['\+\?]~s",$value)) $this->errors[]="There's a not escaped character in the data; string ". $str; //no char except : must be without release character
+			if (preg_match("~\?[:'\+\?]{1}(*SKIP)(*FAIL)|['\+\?]~s",$value)) $this->errors[]="There's a not escaped character in the data; string ". $str; //no char except : without release character
 			$value=str_replace("?","",$value); //delete release char
 			$value=$this->splitData($value);
 		}
 		return $matches;
 	}
 
-	/* split composite data element */
-	function splitData($str)
+	private function splitData($str) //composite data element
 	{
 		$arr=explode(":",$str);
 		if (count($arr)==1) return $str;
 		return $arr;
-	} 
+	}
+
+	//export
+	public function errors()
+	{
+		if(count($this->errors)==0)	return false;
+		return $this->errors;
+	}
+	
+	public function get()
+	{
+		return $this->parsedfile;
+	}
+
+	public function getJson()
+	{
+		return json_encode($this->parsedfile);
+	}
 }
 ?>
