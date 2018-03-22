1c6961e58e6417b11a811571cfef73c5e72f2844
Stefano <sabas88@gmail.com>
2018-03-22 14:31:01 +0100

    Refactor $wrap

--- src/EDI/Encoder.php
+++ src/EDI/Encoder.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * EDIFACT Messages Encoder
- * (c)2016 Stefano Sabatini
+ * (c) 2018 Stefano Sabatini
  */
 
 namespace EDI;
@@ -12,6 +12,7 @@ class Encoder
     private $output = '';
     private $UNAActive = false; // disable by default to preserve backward compatibility
     private $originalArray = [];
+    private $wrap = true; //when false adds a newline after each segment
 
     /**
      * @var string : component separator character (default :)
@@ -55,6 +56,8 @@ class Encoder
     public function encode($arr, $wrap = true, $filterKeys = false)
     {
         $this->originalArray = $arr;
+        $this->wrap = $wrap;
+
         $edistring = '';
         $count = count($arr);
         $k = 0;
@@ -114,20 +117,19 @@ class Encoder
         return str_replace($search, $replace, $str);
     }
 
-    public function get($wrap = true)
+    public function get()
     {
         if ($this->UNAActive) {
-
-            if ($wrap === false) {
-                $this->symbEnd .= "\n";
-            }
-
-            return "UNA" . $this->sepComp .
+            $una = "UNA" . $this->sepComp .
                     $this->sepData .
                     $this->sepDec .
                     $this->symbRel .
                     $this->symbRep .
-                    $this->symbEnd . $this->output;
+                    $this->symbEnd ;
+            if ($this->wrap === false) {
+                $una .= "\n";
+            }
+            return $una . $this->output;
         } else {
             return $this->output;
         }
