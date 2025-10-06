eeafd52823a3c6883f069d641869d01194ec429b
fstuhec <frank.stuhec@secondreality.com>
2025-10-06 14:58:22 +0100

    fix: Reinstate UNOW regex pattern in Parser.php

--- src/EDI/Parser.php
+++ src/EDI/Parser.php
@@ -56,7 +56,7 @@ class Parser
         "UNOA" => "/[\x01-\x1F\x80-\xFF]/", // not as restrictive as it should be
         "UNOB" => "/[\x01-\x1F\x80-\xFF]/",
         "UNOC" => "/[\x01-\x1F\x7F-\x9F]/",
-       // "UNOW" => "/[\x01-\x1F\x7F-\x9F]/",
+        "UNOW" => "/[\x01-\x1F\x7F-\x9F]/",
     ];
 
     /**
